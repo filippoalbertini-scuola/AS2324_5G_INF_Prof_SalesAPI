b89100bcaf53703ea5d16b202b55df1bdb75e519
Filippo Albertini <filippo@filippoalbertini.it>
2024-05-23 15:12:01 +0200

    feat: inizio implementazione GetClients

---
+++ AS2324_5G_INF_Prof_SalesAPI/Controllers/ClientsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SQLite;
+
+namespace AS2324_5G_INF_Prof_SalesAPI.Controllers
+{
+    public class ClientsController : Controller
+    {
+        [HttpGet("GetClients")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DataTable))]
+        public JsonResult GetClients()
+        {
+            DataTable? dtbClients = null;
+
+            string query = "";
+
+            // connessione al DB in SQL Lite (vedi www.connectionstrings.com)
+
+            string strConn = @"Data Source=" + "C:\\Appl\\Scuola\\AS_2023_2024\\5G\\AS2324_5G_INF_Prof_SalesAPI\\AS2324_5G_INF_Prof_SalesAPI\\Database\\northwindITA.db" + ";Pooling=false;Synchronous=Full;"; ;
+
+            SQLiteConnection conn = new SQLiteConnection(strConn);
+            conn.Open();
+
+
+            // carico il data table clienti
+
+            // prepara la QUERY
+            query = "";
+            query = query + "SELECT ";
+            query = query + "   IdCliente, NomeSocieta, Indirizzo ";
+            query = query + "FROM ";
+            query = query + "   Clienti ";
+
+            // crea DataAdapter
+            SQLiteDataAdapter da = new SQLiteDataAdapter(query, conn);
+
+            // popola il DataTable con DataAdapter 
+            dtbClients = new DataTable();
+            da.Fill(dtbClients);
+
+            conn.Close();
+        }
+    }
+}
